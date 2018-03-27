e4d6d43589588e98a13487bd99233e423a3dde36
jessie-zly <610141748@qq.com>
2018-03-27 23:04:57 +0800

    v-html 指令使用时的样式渲染示例

--- src/components/common/AppError.vue
+++ src/components/common/AppError.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="app-error">
     您访问的页面不存在~~
+
+    <div v-html='rmb'></div>
+
+    <div>
+      <span v-html='rmb2'></span>
+      <span class='money'>5000.00</span>
+
+    </div>
+
+    <div v-html='dollar'></div>
   </div>
 </template>
 
 <script>
   export default {
     //
+    data() {
+      return {
+        rmb: '<p>我用v-html 解析的</p>',
+        rmb2: '<b>¥</b>',
+        dollar: '<span>我是用 mustache 解析出来的</span>'
+      }
+    },
   }
 </script>
 
@@ -18,6 +35,21 @@
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+  }
+
+  div > > > b {
+    font: 20px/30px '';
+    color: #0ff;
+  }
+
+  div > > > span {
+    color: #355;
+    font-size: 20px;
+  }
+
+  .money{
+    margin-left: 50px;
+
   }
 
 </style>
